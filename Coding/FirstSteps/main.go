deb59c6528c142ba74526cb1bf4ac2a4d6ca22a4
LordKzoth <nik-kron@mail.ru>
2022-03-14 03:43:53 +0300

    New

---
+++ Coding/FirstSteps/main.go
@@ -0,0 +1,16 @@
+// Объявление нового пакета
+package main
+
+// Импорт пакета
+import "fmt"
+
+func main() { // Entry point
+	var (
+		name, surname string
+	)
+
+	fmt.Println("Hi, who are you?")
+	fmt.Scan(&name, &surname)
+
+	fmt.Println("Hello, " + name + " " + surname)
+}
