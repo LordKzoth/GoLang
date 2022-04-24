304673e10f19023a90a30081d1cf61f06a510fe5
LordKzoth <nik-kron@mail.ru>
2022-04-24 12:58:31 +0300

    5. Longest Palindromic Substring (Medium)

--- Coding/FirstSteps/LeetCode/leetcode.go
+++ Coding/FirstSteps/LeetCode/leetcode.go
@@ -1,81 +1,59 @@
 package leetcode
 
-func FindMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	minimum := func(a, b int) int {
-		if a < b {
-			return a
-		} else {
-			return b
-		}
-	}
-	maximum := func(a, b int) int {
-		if a > b {
-			return a
-		} else {
-			return b
-		}
+func longestPalindrome(s string) string {
+	sLen := len(s)
+
+	dp := make([][]bool, sLen)
+	for i := range dp {
+		dp[i] = make([]bool, sLen)
 	}
 
-	l1, l2 := len(nums1), len(nums2)
+	// Len = 1
+	var maxLen, currentLen, start int
 
-	if l1 == 0 && l2 == 0 {
-		return 0
-	} else if l1 == 0 {
-		if l2%2 == 0 {
-			return float64(nums2[l2/2]+nums2[(l2/2)-1]) / 2
-		} else {
-			return float64(nums2[l2/2])
-		}
-	} else if l2 == 0 {
-		if l1%2 == 0 {
-			return float64(nums1[l1/2]+nums1[(l1/2)-1]) / 2
-		} else {
-			return float64(nums1[l1/2])
-		}
-	}
-
-	if l1 > l2 {
-		return FindMedianSortedArrays(nums2, nums1)
-	}
-
-	max, min := l1, 0
-	var median, i, j int
-
-	for min <= max {
-		i = (min + max) / 2
-		j = (l1+l2+1)/2 - i
-
-		if j < 0 {
-			max = i - 1
-			continue
-		}
-
-		if i < l1 && j > 0 && nums2[j-1] > nums1[i] {
-			min = i + 1
-		} else if i > 0 && j < l2 && nums2[j] < nums1[i-1] {
-			max = i - 1
-		} else {
-			if i == 0 {
-				median = nums2[j-1]
-			} else if j == 0 {
-				median = nums1[i-1]
-			} else {
-				median = maximum(nums1[i-1], nums2[j-1])
+	for j := 0; j < sLen; j++ {
+		for i := j; i < sLen; i++ {
+			if s[i] == s[j] {
+				dp[i][j] = true
 			}
 
-			break
+			if j != i {
+				continue
+			}
+
+			// Set start Len
+			if currentLen == 0 && j >= currentLen+1 && dp[i][j-1] { // EVEN
+				currentLen = 2
+
+			} else if currentLen == 0 && j >= currentLen+2 && dp[i][j-2] { // ODD
+				currentLen = 3
+
+			} else { // Find next point
+				if currentLen != 0 && j >= currentLen+1 && dp[i][j-(currentLen+1)] {
+					currentLen += 2
+				}
+			}
+
+			if currentLen > maxLen {
+				start = i - currentLen + 1
+				maxLen = currentLen
+			} else {
+				if j >= 1 && dp[i][j-1] { // EVEN
+					currentLen = 2
+
+				} else if j >= 2 && dp[i][j-2] { // ODD
+					currentLen = 3
+
+				} else {
+					currentLen = 0
+				}
+			}
 		}
 	}
 
-	if (l1+l2)%2 == 1 {
-		return float64(median)
+	if maxLen == 0 {
+		return string(s[0])
+	} else {
+		return s[start : start+maxLen]
 	}
-	if i == l1 {
-		return float64(median+nums2[j]) / 2
-	}
-	if j == l2 {
-		return float64(median+nums1[i]) / 2
-	}
-
-	return float64(median+minimum(nums1[i], nums2[j])) / 2
 }
--- Coding/FirstSteps/LeetCode/leetcode_test.go
+++ Coding/FirstSteps/LeetCode/leetcode_test.go
@@ -5,34 +5,40 @@ import (
 )
 
 type addTest struct {
-	arg1 []int
-	arg2 []int
-	expected float64
+	arg1     string
+	expected string
 }
 
 var addTests = []addTest {
-	addTest{[]int{1, 3}, []int{2}, 2},
-	addTest{[]int{1, 4}, []int{3, 2}, 2.5},
-	addTest{[]int{1, 3}, []int{}, 2},
-	addTest{[]int{1}, []int{2}, 1.5},
-	addTest{[]int{}, []int{2}, 2},
-	addTest{[]int{}, []int{}, 0},
+	addTest{"dodosso", "osso"},
+	addTest{"babad", "bab"},
+	addTest{"babads", "bab"},
+	addTest{"cbbd", "bb"},
+	addTest{"cbbds", "bb"},
+	addTest{"a", "a"},
+	addTest{"aa", "aa"},
+	addTest{"aba", "aba"},
+	addTest{"dbbd", "dbbd"},
+	addTest{"abcd", "a"},
+	addTest{"dasa", "asa"},
+	addTest{"dassa", "assa"},
+	addTest{"dasaa", "asa"},
 }
 
 // Table-Driven Test
-func TestFindMedianSortedArrays(t *testing.T) {
+func TestLongestPalindrome(t *testing.T) {
 	for _, test := range addTests {
-		if output := FindMedianSortedArrays(test.arg1, test.arg2); output != test.expected {
-			t.Errorf("Output %.2f not equal to expected %.2f", output, test.expected)
+		if output := longestPalindrome(test.arg1); output != test.expected {
+			t.Errorf("Output %q not equal to expected %q", output, test.expected)
 		}
 	}
 }
 
 // Benchmark Test
-func BenchmarkFindMedianSortedArrays(b *testing.B) {
+func BenchmarkLongestPalindrome(b *testing.B) {
 	for _, test := range addTests {
 		for i := 0; i < b.N; i++ {
-			FindMedianSortedArrays(test.arg1, test.arg2)
+			longestPalindrome(test.arg1)
 		}
 	}
 }
